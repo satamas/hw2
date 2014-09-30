65a0dd531ec740b519bc339d02793827db418cb9
satamas <se.atamas@gmail.com>
2014-09-30 16:06:28 +0400

    Indexes starts from 0 now

--- main.cpp
+++ main.cpp
@@ -1,49 +1,48 @@
 #include <iostream>
-#include <string>
 #include "Polygon.h"
 
 using std::cin;
 using std::cout;
 
-void checkTriangulation(Polygon polygon){
+void checkTriangulation(Polygon polygon) {
     int realDoubledSquare = polygon.getDoubledSquare();
     vector<Polygon *> triangulation = polygon.triangulate();
     int doubledSquare = 0;
-    for (Polygon * p : triangulation) {
-        if(p->getSize() != 3){
+    for (Polygon *p : triangulation) {
+        if (p->getSize() != 3) {
             std::cerr << "Not a tringle in the triangulation." << std::endl;
         }
         doubledSquare += p->getDoubledSquare();
     }
-    if(doubledSquare != realDoubledSquare){
-        std::cerr << "Square of triangles is " << doubledSquare << ", but original polygon sqare is " << realDoubledSquare <<std::endl;
+    if (doubledSquare != realDoubledSquare) {
+        std::cerr << "Square of triangles is " << doubledSquare << ", but original polygon sqare is " << realDoubledSquare << std::endl;
     }
 }
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char *argv[]) {
     Polygon polygon;
     cin >> polygon;
 
-    if( (argc > 1) && (std::string(argv[1]) == "-t") ){
+    if ((argc > 1) && (std::string(argv[1]) == "-t")) {
         checkTriangulation(polygon);
     } else {
         for (Polygon *p : polygon.triangulate()) {
-            for(int i = 0; i < 2 ; ++i) {
+            for (int i = 0; i < 2; ++i) {
                 if (p->_vertex->_no > p->_vertex->_next->_no) {
                     p->_vertex = p->_vertex->_next;
                 }
             }
-            for(int i = 0; i < 2 ; ++i) {
+            for (int i = 0; i < 2; ++i) {
                 if (p->_vertex->_no > p->_vertex->_prev->_no) {
                     p->_vertex = p->_vertex->_prev;
                 }
             }
             cout << '(';
-            cout << p->_vertex->_no;
+            cout << p->_vertex->_no - 1;
             cout << ',';
-            cout << p->_vertex->_next->_no;
+            cout << p->_vertex->_next->_no - 1;
             cout << ',';
-            cout << p->_vertex->_next->_next->_no;
+            cout << p->_vertex->_next->_next->_no - 1;
             cout << ')' << std::endl;
         }
     }
